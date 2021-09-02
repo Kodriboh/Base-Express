b855a67ee6afa7636cc5107b8ebe355bfae11ba1
LukeMcCann <developer-lukemccann@outlook.com>
2021-09-02 19:35:11 +0100

    Add basic navbar styling

--- routes/web/pageRoutes.js
+++ routes/web/pageRoutes.js
@@ -3,7 +3,6 @@
 const { appName } = include('/resources/bootstrap/app');
 
 const express = require('express');
-const page = include('/models/page');
 const router = express.Router();
 
 router.get('/', (req, res) => {
